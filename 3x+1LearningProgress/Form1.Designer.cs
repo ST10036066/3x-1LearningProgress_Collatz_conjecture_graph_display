1549371fbae82de57dad26fca3883940697c9128
Azania Aria Ncube <ST10036066@vcconnect.edu.za>
2024-08-04 15:04:42 +0200

    Add project files.

---
+++ 3x+1LearningProgress/Form1.Designer.cs
@@ -0,0 +1,361 @@
+﻿namespace _3x_1LearningProgress
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.charResult = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonProcess = new System.Windows.Forms.Button();
+            this.clearAllBTN = new System.Windows.Forms.Button();
+            this.enterNumbTB = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.tabControl2 = new System.Windows.Forms.TabControl();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.runAllBTN = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.charResult)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.tabControl2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(310, 71);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(200, 100);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(192, 74);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(192, 74);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // charResult
+            // 
+            chartArea5.Name = "ChartArea1";
+            this.charResult.ChartAreas.Add(chartArea5);
+            this.charResult.Dock = System.Windows.Forms.DockStyle.Top;
+            legend5.Name = "Legend1";
+            this.charResult.Legends.Add(legend5);
+            this.charResult.Location = new System.Drawing.Point(0, 0);
+            this.charResult.Name = "charResult";
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series5.Font = new System.Drawing.Font("Constantia", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            series5.LabelForeColor = System.Drawing.Color.RosyBrown;
+            series5.Legend = "Legend1";
+            series5.Name = "Series1_3x+1";
+            this.charResult.Series.Add(series5);
+            this.charResult.Size = new System.Drawing.Size(800, 300);
+            this.charResult.TabIndex = 1;
+            this.charResult.Text = "chart1";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.buttonProcess);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.clearAllBTN);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.enterNumbTB);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.tabControl2);
+            this.panel1.Controls.Add(this.runAllBTN);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 300);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 150);
+            this.panel1.TabIndex = 2;
+            // 
+            // buttonProcess
+            // 
+            this.buttonProcess.Location = new System.Drawing.Point(413, 115);
+            this.buttonProcess.Name = "buttonProcess";
+            this.buttonProcess.Size = new System.Drawing.Size(137, 23);
+            this.buttonProcess.TabIndex = 5;
+            this.buttonProcess.Text = "Generate Sequence";
+            this.buttonProcess.UseVisualStyleBackColor = true;
+            this.buttonProcess.Click += new System.EventHandler(this.buttonProcess_Click);
+            // 
+            // clearAllBTN
+            // 
+            this.clearAllBTN.Location = new System.Drawing.Point(310, 115);
+            this.clearAllBTN.Name = "clearAllBTN";
+            this.clearAllBTN.Size = new System.Drawing.Size(75, 23);
+            this.clearAllBTN.TabIndex = 2;
+            this.clearAllBTN.Text = "CLEAR";
+            this.clearAllBTN.UseVisualStyleBackColor = true;
+            this.clearAllBTN.Click += new System.EventHandler(this.clearAllBTN_Click);
+            // 
+            // enterNumbTB
+            // 
+            this.enterNumbTB.BackColor = System.Drawing.Color.RosyBrown;
+            this.enterNumbTB.Location = new System.Drawing.Point(270, 89);
+            this.enterNumbTB.Name = "enterNumbTB";
+            this.enterNumbTB.Size = new System.Drawing.Size(211, 20);
+            this.enterNumbTB.TabIndex = 4;
+            this.enterNumbTB.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label9);
+            this.panel2.Controls.Add(this.label8);
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(600, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 150);
+            this.panel2.TabIndex = 3;
+            // 
+            // tabControl2
+            // 
+            this.tabControl2.Controls.Add(this.tabPage3);
+            this.tabControl2.Controls.Add(this.tabPage4);
+            this.tabControl2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.tabControl2.Location = new System.Drawing.Point(0, 0);
+            this.tabControl2.Name = "tabControl2";
+            this.tabControl2.SelectedIndex = 0;
+            this.tabControl2.Size = new System.Drawing.Size(200, 150);
+            this.tabControl2.TabIndex = 0;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.label4);
+            this.tabPage3.Controls.Add(this.label1);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(192, 124);
+            this.tabPage3.TabIndex = 0;
+            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.DarkOrange;
+            this.label2.Font = new System.Drawing.Font("Lucida Sans Typewriter", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label2.Location = new System.Drawing.Point(267, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Enter a number ";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.SandyBrown;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 18F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(53, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "3X + 1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(192, 124);
+            this.tabPage4.TabIndex = 1;
+            this.tabPage4.Text = "tabPage4";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // runAllBTN
+            // 
+            this.runAllBTN.Location = new System.Drawing.Point(215, 115);
+            this.runAllBTN.Name = "runAllBTN";
+            this.runAllBTN.Size = new System.Drawing.Size(75, 23);
+            this.runAllBTN.TabIndex = 3;
+            this.runAllBTN.Text = "RUN ALL";
+            this.runAllBTN.UseVisualStyleBackColor = true;
+            this.runAllBTN.Click += new System.EventHandler(this.RunBtn_click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.DarkOrange;
+            this.label3.Font = new System.Drawing.Font("Lucida Sans Typewriter", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(379, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(215, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "and click \'Generate Sequence\' ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 46);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.DarkOrange;
+            this.label5.Font = new System.Drawing.Font("Lucida Sans Typewriter", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(267, 46);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(147, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "to see how the 3x+1 ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.DarkOrange;
+            this.label6.Font = new System.Drawing.Font("Lucida Sans Typewriter", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(433, 46);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(117, 12);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "sequence evolves";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 11);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(67, 13);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "The buttons:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(3, 36);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(173, 13);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "RUN ALL=saves your number";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(3, 68);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(143, 13);
+            this.label9.TabIndex = 2;
+            this.label9.Text = "CLEAR= clears numbers";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.charResult);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.charResult)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.tabControl2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart charResult;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TabControl tabControl2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button clearAllBTN;
+        private System.Windows.Forms.Button runAllBTN;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox enterNumbTB;
+        private System.Windows.Forms.Button buttonProcess;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ 3x+1LearningProgress/My3XPlus1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3x_1LearningProgress
+{
+    public class My3XPlus1
+    {
+        /// <summary>
+        /// list holds the data generated 
+        /// </summary>
+
+        private readonly List<long> DataList; //after calculation 
+
+        private Form1 Form1;
+
+        public List<long> DataListPublic => this.DataList;
+
+        /// <summary>
+        /// default constructor 
+        /// </summary>
+        public My3XPlus1()
+        {
+            this.DataList = new List<long>(); //ignore
+
+        }
+        //------------------------------------------------------------------------------------------
+        /// <summary>
+        /// 
+        /// </summary>
+        /// basically the disctionary helps store and move the saved number (calculated) 
+        /// <summary>
+        /// Calculates the 3x + 1 sequence for each number in the provided list.
+        /// </summary>
+        public Dictionary<int, List<int>> CalculateForList(List<int> numbers)
+        {
+            var results = new Dictionary<int, List<int>>();
+
+            foreach (var number in numbers)
+            {
+                results[number] = Calculate(number);
+            }
+
+            return results; 
+        }
+
+        //----------------------------------------------------------------------------------------
+        /// <summary>
+        /// Method that takes in number from data structure (textbox input) and sends it to formula method
+        /// Calculate
+        /// </summary>
+        /// <summary>
+        /// Calculates the 3x + 1 sequence for a single number.
+        /// </summary>
+        public List<int> Calculate(int number)
+        {
+            List<int> results = new List<int>(); //adds final answer to data structure
+
+            while (number != 1) //this is when the famous calculation starts
+            {
+                results.Add(number);
+                if (number % 2 == 0) //if the number is even , divide
+                {
+                    number /= 2;
+                }
+                else
+                {
+                    number = 3 * number + 1; //else , if odd , multiple by 3
+                }
+            }
+            results.Add(1); // Add the final 1 to the list
+
+            return results; //returns the answer for graph displaying
+        }
+
+        
+
+        
+    }
+}
+
+  
+
+   
+
